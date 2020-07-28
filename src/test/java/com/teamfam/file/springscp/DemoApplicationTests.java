6209a34f994cb4d14462c38bc18b9708f34afcb2
teamfam <teamfam2014@gmail.com>
2020-07-28 17:39:53 -0400

    added initial version

---
+++ src/test/java/com/teamfam/file/springscp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfam.file.springscp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
