3577b6934de8569521b59eae3813baaa34944af7
teamfam <teamfam2014@gmail.com>
2020-07-29 12:24:57 -0400

    Added config props

---
+++ src/main/java/com/teamfam/file/springscp/config/ScpConfigurationProperties.java
@@ -0,0 +1,57 @@
+package com.teamfam.file.springscp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties of the SCP transmission such as the key file location, the keyfile password etc.
+ * 
+ * @author teamfam
+ */
+@ConfigurationProperties(prefix = "revstream")
+public class ScpConfigurationProperties {
+    private String keyFilePath;
+    private String keyPassword;
+    private String userName;
+    private String host;
+    private String port;
+
+    public String getKeyFilePath() {
+        return keyFilePath;
+    }
+
+    public void setKeyFilePath(String keyFilePath) {
+        this.keyFilePath = keyFilePath;
+    }
+
+    public String getKeyPassword() {
+        return keyPassword;
+    }
+
+    public void setKeyPassword(String keyPassword) {
+        this.keyPassword = keyPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+}
