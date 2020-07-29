f7c93262944a8bfb8eb19b1eeacf0cd6553baa88
teamfam <teamfam2014@gmail.com>
2020-07-29 11:00:50 -0400

    Add Auto Config

---
+++ src/main/java/com/teamfam/file/springscp/config/ScpAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.teamfam.file.springscp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto configure the SCP service library.
+ * 
+ * @author teamfam
+ */
+@Configuration
+public class ScpAutoConfiguration {
+    
+}
