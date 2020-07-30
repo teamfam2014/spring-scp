f4c2433a2912bae0fb7e1e995a2ace3a16aa2f7c
teamfam <teamfam2014@gmail.com>
2020-07-30 18:39:06 -0400

    added new ABT

---
+++ src/test/java/com/teamfam/file/springscp/abt/ScpFileTransmitterAppBehaviourTest.java
@@ -0,0 +1,9 @@
+package com.teamfam.file.springscp.abt;
+
+/**
+ * Application behaviour test that will test out sending the file to a destination through
+ * SCP.
+ */
+public class ScpFileTransmitterAppBehaviourTest {
+    
+}
