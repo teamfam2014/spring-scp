7dd3d72e57740f8bbda8eee1c09a489ce7ece4ea
teamfam <teamfam2014@gmail.com>
2020-07-30 23:57:24 -0400

    dummy app for spring boot testing

---
+++ src/test/java/com/teamfam/file/springscp/SpringBootDummyApp.java
@@ -0,0 +1,11 @@
+package com.teamfam.file.springscp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Dummy application used for spring boot testing
+ */
+@SpringBootApplication
+public class SpringBootDummyApp {
+    
+}
