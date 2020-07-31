feabd9909e32e48e41abde85a6c4098398485bcc
teamfam <teamfam2014@gmail.com>
2020-07-30 23:42:28 -0400

    added spring components

--- src/test/java/com/teamfam/file/springscp/abt/ScpFileTransmitterAppBehaviourTest.java
+++ src/test/java/com/teamfam/file/springscp/abt/ScpFileTransmitterAppBehaviourTest.java
@@ -1,9 +1,14 @@
 package com.teamfam.file.springscp.abt;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 /**
- * Application behaviour test that will test out sending the file to a destination through
- * SCP.
+ * Application behaviour test that will test out sending the file to a
+ * destination through SCP.
  */
+@SpringBootTest
+@ActiveProfiles("test")
 public class ScpFileTransmitterAppBehaviourTest {
     
 }
