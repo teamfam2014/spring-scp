50732761be40521deb7e675dbf3c85631aa57eed
teamfam <teamfam2014@gmail.com>
2020-07-31 00:40:32 -0400

    Added new JSchConfiguration

---
+++ src/main/java/com/teamfam/file/springscp/transmit/config/JSchConfiguration.java
@@ -0,0 +1,17 @@
+package com.teamfam.file.springscp.transmit.config;
+
+import com.jcraft.jsch.JSch;
+
+/**
+ * Configure the JSch for file transmission.
+ */
+public interface JSchConfiguration {
+   
+    /**
+     * Configure the JSch prior to use. Can be used to configure
+     * the JSch with private keys for example.
+     * 
+     * @param jsch The JSch instance to configure.
+     */
+    public void configure(JSch jsch);
+}
