196cade8bbbc747fbc01bfbb2918375ba03a0904
teamfam <teamfam2014@gmail.com>
2020-08-03 01:29:23 -0400

    adjusted for private keys

--- src/test/java/com/teamfam/file/springscp/core/config/JSchConfigurerImplTest.java
+++ src/test/java/com/teamfam/file/springscp/core/config/JSchConfigurerImplTest.java
@@ -1,6 +1,8 @@
 package com.teamfam.file.springscp.core.config;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -29,58 +31,98 @@ public class JSchConfigurerImplTest {
     private ScpConfigurationProperties scpConfigurationProperties;
 
     /**
-     * If the key file path and the key password are empty, then do not populate the
+     * If the public and private key file path and the key password are empty, then do not populate the
      * JSch identity
      * 
      * @throws JSchException
      */
-    @DisplayName("Key File Path and Key Password empty")
+    @DisplayName("Pub and Priv Key File Path, and Key Password empty")
     @Test
-    public void bothEmpty() throws JSchException {
+    public void allEmpty() throws JSchException {
         //ARRANGE
         JSch jsch = mock(JSch.class);
         //ACT
         jschConfigurerImpl.configure(jsch);
         //ASSERT
+        verify(jsch,times(0)).addIdentity(anyString(),anyString(),any(byte[].class));
         verify(jsch,times(0)).addIdentity(anyString(),anyString());
-        verify(jsch,times(0)).addIdentity(anyString());
     }
 
     /**
-     * If the key file path is populated but the key password is empty
-     * then only populate the JSch identity with the key file path.
+     * If private key is populated, but public key is not populated, then do not populate
+     * anything.
      */
-    @DisplayName("Key File Path populated only")
+    @DisplayName("Priv Key File Path populated only")
     @Test
-    public void keyFilePathPopulated() throws JSchException {
+    public void prvPopulated() throws JSchException {
         //ARRANGE
         JSch jsch = mock(JSch.class);
-        when(scpConfigurationProperties.getKeyFilePath())
+        when(scpConfigurationProperties.getPrvKeyFilePath())
+        .thenReturn("docker/ssh_host_rsa_key");          
+        //ACT
+        jschConfigurerImpl.configure(jsch);
+        //ASSERT
+        verify(jsch,times(0)).addIdentity(anyString(),anyString(),any(byte[].class));
+        verify(jsch,times(0)).addIdentity(anyString(),anyString());
+    }    
+    
+    /**
+     * If public key is populated, but private key is not populated, then do not populate
+     * anything.
+     */
+    @DisplayName("Pub Key File Path populated only")
+    @Test
+    public void pubPopulated() throws JSchException {
+        //ARRANGE
+        JSch jsch = mock(JSch.class);
+        lenient().when(scpConfigurationProperties.getPubKeyFilePath())
+        .thenReturn("docker/ssh_host_rsa_key.pub");          
+        //ACT
+        jschConfigurerImpl.configure(jsch);
+        //ASSERT
+        verify(jsch,times(0)).addIdentity(anyString(),anyString(),any(byte[].class));
+        verify(jsch,times(0)).addIdentity(anyString(),anyString());
+    }    
+
+    /**
+     * If the private and public key file path is populated but the key password is empty
+     * then only populate the JSch identity with the key file path.
+     */
+    @DisplayName("Public and Private Key File Path populated only")
+    @Test
+    public void keyFilePathsPopulated() throws JSchException {
+        //ARRANGE
+        JSch jsch = mock(JSch.class);
+        when(scpConfigurationProperties.getPrvKeyFilePath())
+        .thenReturn("docker/ssh_host_rsa_key");        
+        when(scpConfigurationProperties.getPubKeyFilePath())
         .thenReturn("docker/ssh_host_rsa_key.pub");
         //ACT
         jschConfigurerImpl.configure(jsch);
         //ASSERT
-        verify(jsch,times(0)).addIdentity(anyString(),anyString());
-        verify(jsch,times(1)).addIdentity(anyString());
+        verify(jsch,times(0)).addIdentity(anyString(),anyString(),any(byte[].class));
+        verify(jsch,times(1)).addIdentity(anyString(),anyString());
     }    
 
     /**
-     * If the key file path and the key password are populated then
+     * If the public and private key file path and the key password are populated then
      * populate both in the JSch identify.
      */
-    @DisplayName("Key File Path and Key Password populated")
+    @DisplayName("All Paths and Pwd populated")
     @Test
-    public void bothPopulated() throws JSchException {
+    public void allPopulated() throws JSchException {
         //ARRANGE
         JSch jsch = mock(JSch.class);
-        when(scpConfigurationProperties.getKeyFilePath())
+        when(scpConfigurationProperties.getPrvKeyFilePath())
+        .thenReturn("docker/ssh_host_rsa_key");        
+        when(scpConfigurationProperties.getPubKeyFilePath())
         .thenReturn("docker/ssh_host_rsa_key.pub");
         when(scpConfigurationProperties.getKeyPassword())
         .thenReturn("test-Pwd");        
         //ACT
         jschConfigurerImpl.configure(jsch);
         //ASSERT
-        verify(jsch,times(1)).addIdentity(anyString(),anyString());
-        verify(jsch,times(0)).addIdentity(anyString());
+        verify(jsch,times(1)).addIdentity(anyString(),anyString(),any(byte[].class));
+        verify(jsch,times(0)).addIdentity(anyString(),anyString());
     }    
 }
